4ceb346cbee3236341ed8754a2132b51606380d6
iwairikuto <wjrikuto@gmial.com>
2022-12-03 22:29:04 +0900

    routerをvue3に対応

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,44 @@
-import Vue from 'vue'
-import Router from 'vue-router'
 import HelloWorld from '@/components/pages/HelloWorld'
 import IndexPage from '@/components/pages/IndexPage'
 import AtomsPage from '@/components/pages/AtomsPage'
 import MoleculesPage from '@/components/pages/MoleculesPage'
 import OrganismsPage from '@/components/pages/OrganismsPage'
+import { createRouter, createWebHistory } from 'vue-router'
 
-Vue.use(Router)
+const routes = [
+  {
+    path: '/',
+    name: 'IndexPage',
+    component: IndexPage
+  },
+  {
+    path: '/atoms',
+    name: 'AtomsPage',
+    component: AtomsPage
+  },
+  {
+    path: '/molecules',
+    name: 'MoleculesPage',
+    component: MoleculesPage
+  },
+  {
+    path: '/organisms',
+    name: 'OrganismsPage',
+    component: OrganismsPage
+  },
+  {
+    path: '/hello',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }
+]
 
-export default new Router({
-  base: '/vue-atomic-design-template/',
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'IndexPage',
-      component: IndexPage
-    },
-    {
-      path: '/atoms',
-      name: 'AtomsPage',
-      component: AtomsPage
-    },
-    {
-      path: '/molecules',
-      name: 'MoleculesPage',
-      component: MoleculesPage
-    },
-    {
-      path: '/organisms',
-      name: 'OrganismsPage',
-      component: OrganismsPage
-    },
-    {
-      path: '/hello',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
 })
+
+export default router
+
+
+
